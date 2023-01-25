237a9fa48ac2f53106096b299597e97023db1c7e
Nurullo Sulaymonov <48081398+NurulloSulaymonov@users.noreply.github.com>
2023-01-25 18:45:29 +0500

    some changes

--- Domain/Dtos/StudentDto.cs
+++ Domain/Dtos/StudentDto.cs
@@ -6,7 +6,7 @@ public class StudentDto
 {
     public int Id { get; set; }
     public string Email { get; set; }
-    public string Gender { get; set; }
+    public Gender Gender { get; set; }
     public string FullName { get; set; }
     public DateTime BirthDate { get; set; }
     public string Address { get; set; }
